d799a0da2c5bab22f603520cc1fad4552d5b7f1e
Julian Veit <claymore1298@gmail.com>
2018-12-04 23:51:20 +0100

    FlipFlap: update for the new base
    Change-Id: I5681d7e175929e218ccc9cad1a10956dd31baa9e

--- Android.mk
+++ Android.mk
@@ -9,9 +9,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := FlipFlap
@@ -23,8 +20,7 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/res
 
 LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.flipflap.xml
 
